f87ac5db1ae93797f3fc938071faebc1f1f5665b
hurzstore <hurz.store@gmail.com>
2021-09-16 02:39:59 +0300

    it works

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 const commerce = require('./commerce.config.json')
-const {
-  withCommerceConfig,
-  getProviderName,
-} = require('./framework/commerce/config')
-
-const provider = commerce.provider || getProviderName()
-const isBC = provider === 'bigcommerce'
-const isShopify = provider === 'shopify'
-const isSaleor = provider === 'saleor'
-const isSwell = provider === 'swell'
-const isVendure = provider === 'vendure'
+const { withCommerceConfig } = require('./framework/commerce/config')
 
 module.exports = withCommerceConfig({
   commerce,
@@ -19,23 +9,14 @@ module.exports = withCommerceConfig({
   },
   rewrites() {
     return [
-      (isBC || isShopify || isSwell || isVendure) && {
+      {
         source: '/checkout',
         destination: '/api/checkout',
       },
-      // The logout is also an action so this route is not required, but it's also another way
-      // you can allow a logout!
-      isBC && {
+      {
         source: '/logout',
         destination: '/api/logout?redirect_to=/',
       },
-      // For Vendure, rewrite the local api url to the remote (external) api url. This is required
-      // to make the session cookies work.
-      isVendure &&
-        process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL && {
-          source: `${process.env.NEXT_PUBLIC_VENDURE_LOCAL_URL}/:path*`,
-          destination: `${process.env.NEXT_PUBLIC_VENDURE_SHOP_API_URL}/:path*`,
-        },
     ].filter(Boolean)
   },
 })
