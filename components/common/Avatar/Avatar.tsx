373edff0ff144a97d6a682b30047fbd7383e01f6
zvirinz <zvirinz@outlook.com>
2021-11-26 00:17:03 +0300

    small fixes + avatar

--- components/common/Avatar/Avatar.tsx
+++ components/common/Avatar/Avatar.tsx
@@ -11,13 +11,27 @@ const Avatar: FC<Props> = ({}) => {
   let { userAvatar } = useUserAvatar()
 
   return (
-    <div
-      ref={ref}
-      style={{ backgroundImage: userAvatar }}
-      className="inline-block h-8 w-8 rounded-full border-2 border-primary hover:border-secondary focus:border-secondary transition-colors ease-linear"
+    // <div
+    //   ref={ref}
+    //   style={{ backgroundImage: userAvatar }}
+    //   className="inline-block w-8 h-8 transition-colors ease-linear border-2 rounded-full border-primary hover:border-secondary focus:border-secondary"
+    // >
+    //   {/* Add an image - We're generating a gradient as placeholder  <img></img> */}
+    // </div>
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="var(--secondary)"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Add an image - We're generating a gradient as placeholder  <img></img> */}
-    </div>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.75,15.67a6,6,0,1,0-7.51,0A11,11,0,0,0,5,26v1H27V26A11,11,0,0,0,19.75,15.67ZM12,11a4,4,0,1,1,4,4A4,4,0,0,1,12,11ZM7.06,25a9,9,0,0,1,17.89,0Z"
+        fill="var(--secondary)"
+      />
+    </svg>
   )
 }
 
